d312161f15bff92c39e33376538f389649a8442f
Fizzuz <mhafizgpr@gmail.com>
2025-10-07 16:20:22 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS17.java
+++ jobsheet5/ifCetakKRS17.java
@@ -7,14 +7,8 @@ public class ifCetakKRS17{
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if(uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Pembayaran UKT belum terverifikasi";
 
-        }
-        else{
-            System.out.println("Pembayaran UKT belum terverifikasi");
-            
-        }
+        System.out.println(pesan);
     }
 }
