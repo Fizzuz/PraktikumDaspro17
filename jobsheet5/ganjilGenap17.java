787d7025fb362b355a253ac1ae46905bfe6af25a
Fizzuz <mhafizgpr@gmail.com>
2025-10-07 16:52:36 +0700

    Tugas 2

---
+++ jobsheet5/ganjilGenap17.java
@@ -0,0 +1,21 @@
+package ganjilGenap17;
+import java.util.Scanner;
+public class ganjilGenap17 {
+    public static void main(String[] args) {
+        int angka;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        angka = sc.nextInt();
+
+        if (angka %2 == 0){
+            System.out.println("Genap");
+        }
+        else {
+            System.out.println("Ganjil");
+        }
+
+    }
+    
+    
+}
