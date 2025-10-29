801d001ada1a8acbb2c21a0eeb42a6a1b3fbe0e5
Fizzuz <mhafizgpr@gmail.com>
2025-10-29 08:15:50 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor17.java
+++ jobsheet7/SiakadFor17.java
@@ -5,9 +5,9 @@ public class SiakadFor17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
-
-        for (int i = 1; i <=10; i++){
+        double nilai = 0, tertinggi = 0, terendah = 100 ;
+        int i = 1, lulus = 0, tidakLulus = 0;
+        for (i = 1; i <=10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
 
@@ -17,8 +17,18 @@ public class SiakadFor17 {
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai > 60){
+                lulus++;
+            }
+            else {
+                tidakLulus++;
+            }
         }
+        
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " +terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
+        }
     }
-}
+
