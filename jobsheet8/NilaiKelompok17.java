f913743e3e16476ff434064b5288b3fb1bdb5cfc
Fizzuz <mhafizgpr@gmail.com>
2025-11-04 21:21:02 +0700

    Modifikasi Percobaan 4

--- jobsheet8/NilaiKelompok17.java
+++ jobsheet8/NilaiKelompok17.java
@@ -4,8 +4,8 @@ public class NilaiKelompok17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-    float totalNilai, rataNilai;
-    int nilai, i = 1;
+    float totalNilai, rataNilai, rataTertinggi = 0;
+    int nilai, i = 1, kelompokPemenang = 0;
     
 while (i <= 6){
     System.out.println("Kelompok " +i);
@@ -18,7 +18,13 @@ while (i <= 6){
     }
     rataNilai = totalNilai / 5;
     System.out.println("Kelompok " + i + " : nilai rata-rata = " +rataNilai);
+    if (rataNilai > rataTertinggi){
+        rataTertinggi = rataNilai;
+        kelompokPemenang = i;
+    }
     i++;
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah: Kelompok " + kelompokPemenang);
+        System.out.println("Dengan nilai rata-rata: " + rataTertinggi);
     }
 }
