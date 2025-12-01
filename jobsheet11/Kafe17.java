eeb7f4371ce6d296f582c25c7f5a92ce457aaf5c
Fizzuz <mhafizgpr@gmail.com>
2025-12-01 20:58:07 +0700

    Percobaan 3

--- jobsheet11/Kafe17.java
+++ jobsheet11/Kafe17.java
@@ -1,9 +1,9 @@
 package jobsheet11;
-
+import java.util.Scanner;
 public class Kafe17 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat Datang, " +namaPelanggan + "!");
-
+        
         if (isMember){
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
@@ -17,7 +17,7 @@ public class Kafe17 {
             System.out.println("Kode promo tidak valid");
         }
         System.out.println("==== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("1. Kopi Hitam - Rp 15000");
         System.out.println("2. Cappucino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
@@ -26,9 +26,27 @@ public class Kafe17 {
         System.out.println("===================================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
+    public static int hitungTotalHarga17(int pilihanMenu, int banyakItem){
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Menu("Andi", true, "DISKON50");
         Menu("Budi", true, "DISKON30");
+        
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga17(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" +totalHarga);
+
     }
+
     
 }
