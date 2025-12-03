4447066e5776a05885a317be42b578110cfd62e7
Fizzuz <mhafizgpr@gmail.com>
2025-12-03 23:30:10 +0700

    Tugas 1

---
+++ jobsheet11/Tugas1.java
@@ -0,0 +1,32 @@
+package jobsheet11;
+import java.util.Scanner;
+
+public class Tugas1 {
+
+    
+    public double hitungVolume(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    
+    public double hitungLuasPermukaan(double s) {
+        return 6 * (s * s);
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Tugas1 kubus = new Tugas1();
+        
+       
+        System.out.print("Masukkan panjang sisi kubus: ");
+        double sisi = input.nextDouble();
+
+        double v = kubus.hitungVolume(sisi);
+        double lp = kubus.hitungLuasPermukaan(sisi);
+
+        System.out.println("Volume         : " + v);
+        System.out.println("Luas Permukaan : " + lp);
+    }
+}
+
